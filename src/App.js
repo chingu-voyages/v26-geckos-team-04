1042f992f8b968d564fa05f0ee50b72e4d770a17
Yumi Patton <yumipatton@gmail.com>
2021-01-24 21:41:46 +1100

    commit before updating

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import React from "react";
+// import Header from './Header';
+// import Home from './Home';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      hi
-    </div>
-  );
+  return 
+  <Router>
+  <div className="App">
+  <Switch>
+    <Route path="/">
+  {/* <Header />
+      <Home /> */}      
+    </Route>
+  </Switch>
+
+      </div>;    
+  </Router>
+
 }
 
 export default App;
