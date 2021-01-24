c4983589b7a3bd3e05a7ac043f8a4602a082147c
Yumi Patton <yumipatton@gmail.com>
2021-01-24 16:58:54 +1100

    .env

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCaSpYNaWGiFsscNhrMF_oqikPO005lzbk",
-  authDomain: "gecko-team-04.firebaseapp.com",
-  projectId: "gecko-team-04",
-  storageBucket: "gecko-team-04.appspot.com",
-  messagingSenderId: "1001477519875",
-  appId: "1:1001477519875:web:6c143296b3357dac476c36",
-  measurementId: "G-BLT42E6JGR",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
