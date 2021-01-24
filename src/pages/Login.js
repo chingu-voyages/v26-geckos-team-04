9c1ac1650f177c447b122f89e603af76bed9793e
Yumi Patton <yumipatton@gmail.com>
2021-01-24 17:18:13 +1100

    moved login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import { auth } from "../../firebase";
+import { auth } from "../firebase";
 // import { useHistory } from "react-router-dom";
 
 const LoginStyles = styled.div`
