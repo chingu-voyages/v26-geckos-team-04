f67741366e1ebbfebc214ecea5810dfb33541798
Yumi Patton <yumipatton@gmail.com>
2021-01-25 11:52:06 +1100

    updating

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
-
-import Login from "./pages/Login"
+import React from "react";
+// import Header from './components/Header/Header';
+// import Home from "./pages/Home";
+import Product from "./pages/Product";
+// import ShoppingCart from "./pages/ShoppingCart";
+import Login from "./pages/Login";
+// import Login from "./pages/Orders";
+// import Login from "./pages/Payment";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/orders">
+            {/* <Header />  */}
+            {/* <Orders /> */}
+            <h1>Orders</h1>
+          </Route>
+          <Route path="/login">
+            <Login />
+            <h1>Log in</h1>
+          </Route>
+          <Route path="/checkout">
+            {/* <Header />  */}
+            {/* <ShoppingCart /> */}
+            <h1>Shopping Cart</h1>
+          </Route>
+          <Route path="/product">
+            {/* <Header />  */}
+            <Product />
+          </Route>
+          <Route path="/payment">
+            {/* <Header />  */}
+            {/* <Payment /> */}
+            <h1>Payment</h1>
+          </Route>
+          <Route path="/">
+            {/* <Header />  */}
+            {/* <Home /> */}
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
