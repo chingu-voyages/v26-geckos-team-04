1b8df853f76a14a73f8348bba963f26e6de6eab6
Peter Hanley <hanley.peter@gmail.com>
2021-01-28 16:24:18 +0800

    Modified Product Preview to hold dynamic data from Product Context

--- src/components/ProductPreview/ProductPreview.js
+++ src/components/ProductPreview/ProductPreview.js
@@ -1,36 +1,38 @@
 import React from "react";
 import styled from "styled-components";
-import sampleImage from "../../assets/sample-preview.jpg"
 
 const ProductPreviewStyles = styled.div`
 
-    box-sizing: border-box;
-    background-color: white;
-    padding: 10px 20px;
-    width: 100%;
-    z-index: 2;
     display: flex;
     flex-direction: column;
-
+    width: 100%;
+    height: 100%;
+    
     h3 {
         font-size: 21px;
+    }
+
+    h3, p {
+        color: var(--dark-grey);
     }
 
     img {
         margin: 8px 0;
         align-self: center;
+        max-height: 70%;
+        width: 100%;
+        object-fit: contain;
+        flex: 1;
     }
 `;
 
-export default function ProductPreview() {
+export default function ProductPreview({ image, title }) {
 
     return (
         <ProductPreviewStyles>
-            
-            <h3>Recently viewed</h3>
-            <img src={sampleImage} alt="product" />
-            <p>Shop now</p>
-            
+                <h3>Recently viewed</h3>
+                <img src={image} alt={title} />
+                <p>Shop now</p>
         </ProductPreviewStyles>
     )
 }
