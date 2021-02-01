a69a39a699128ec9519893c5352f79f74677982a
Peter Hanley <hanley.peter@gmail.com>
2021-02-01 11:40:59 +0800

    Fixed responsiveness & adjusted styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,140 +1,138 @@
 import React from "react";
-import { Search, ShoppingBasket } from "@material-ui/icons";
+import { Search } from "@material-ui/icons";
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import styled from "styled-components";
 import { Link } from 'react-router-dom'
+
 const HeaderStyles = styled.div`
-  height: 60px;
-  display: flex;
+  min-height: 100px;
+  display: grid;
+  grid-template-columns: auto auto;
+  grid-template-areas: "logo nav" "search search";
   align-items: center;
-  background-color: #131921;
+  background-color: var(--dark-grey);
   position: sticky;
   top: 0;
   z-index: 100;
+  padding: 0 10px 10px 10px;
 
-  .header__logo {
+  .logo {
     width: 100px;
     object-fit: contain;
-    margin: 18px 20px 0;
+    margin-top: 13px;
+    margin-bottom: 7px;
   }
 
-  .header__search {
-    display: flex;
-    flex: 1;
-    align-items: center;
-    border-radius: 24px;
-  }
-
-  .header__searchInput {
-    height: 12px;
-    padding: 10px;
-    border: none;
-    width: 100%;
-  }
-  .header__searchIcon {
-    padding: 5px;
-    height: 22px !important;
-    background-color: #cd9042;
-  }
-
-  .header__option {
-    display: flex;
-    flex-direction: column;
-    margin-left: 10px;
-    margin-right: 10px;
-    color: white;
-  }
-  .header__optionLineOne {
-    font-size: 10px;
-  }
-  .header__optionLineTwo {
-    font-size: 13px;
-    font-weight: 800;
-  }
-
-  .header__optionBasket {
+  .search {
+    grid-area: search;
     display: flex;
     align-items: center;
-    color: white;
+    border-radius: 5px;
+    background-color: white;
+
+    .searchInput {
+      height: 12px;
+      padding: 10px;
+      border: none;
+      width: 100%;
+      background-color: transparent;
+    }
+    .searchIcon {
+      padding: 5px;
+      height: 22px;
+      background-color: var(--orange-1);
+      border-radius: 5px;
+    }
   }
 
-  .header__basketCount {
-    margin-left: 10px;
-    margin-right: 10px;
-  }
-
-  .header__nav {
+  .nav {
     display: flex;
-    justify-content: space-evenly;
+    justify-content: flex-end;
+
+    a {
+      text-decoration: none;
+
+      .option {
+        display: flex;
+        flex-direction: column;
+        margin-left: 10px;
+        margin-right: 10px;
+        color: white;
+
+        .optionLineOne {
+          font-size: 10px;
+        }
+
+        .optionLineTwo {
+          font-size: 13px;
+          font-weight: 800;
+        }
+      }
+
+      .optionBasket {
+        display: flex;
+        height: 100%;
+        align-items: center;
+        color: white;
+        margin-left: 10px;
+
+        .basketCount {
+          margin-left: 5px;
+          margin-right: 5px;
+        }
+      }
+    }
   }
 
-  @media screen and (max-width: 767.98px) {
-    height: 100px;
-
-    .header__logo {
-      position: absolute;
-      top: 10px;
-      width: 60px;
-    }
-    .header__search {
-      height: 60px;
-      position: absolute;
-      width: 95%;
-      margin: auto 2.5%;
-      bottom: 0;
-    }
-    .header__option {
-      margin-bottom: 10px;
-      color: white;
-    }
-    .header__optionLineOne {
-      font-size: 8px;
-    }
-
-    .header__optionLineTwo {
-      font-size: 12px;
-    }
-    .header__nav {
-      position: absolute;
-      top: 10px;
-      right: 5px;
+  @media (min-width: 768px) {
+    min-height: 60px;
+    grid-template-columns: auto 1fr auto;
+    grid-template-areas: "logo search nav";
+    padding: 0 20px;
+    grid-gap: 20px;
+  
+    .nav {
+      justify-content: space-evenly;
     }
   }
 `;
+
+
 function Header() {
   return (
     <HeaderStyles>
       <Link to="/">
         <img
-          className="header__logo"
+          className="logo"
           src="https://mikekitko.com/wp-content/uploads/2019/10/amazon-logo-white-768x232.png"
           alt="Amazon's logo"
         />
       </Link>
-      <div className="header__search">
-        <input className="header__searchInput" type="text" />
-        <Search className="header__searchIcon" />
+      <div className="search">
+        <input className="searchInput" type="text" />
+        <Search className="searchIcon" />
       </div>
-      <div className="header__nav">
+      <div className="nav">
         <Link to="/login">
-          <div className="header__option">
-            <span className="header__optionLineOne">Hello Guest</span>
-            <span className="header__optionLineTwo">Sign in</span>
+          <div className="option">
+            <span className="optionLineOne">Hello Guest</span>
+            <span className="optionLineTwo">Sign in</span>
           </div>
         </Link>
         <Link to="/orders">
-          <div className="header__option">
-            <span className="header__optionLineOne">Returns</span>
-            <span className="header__optionLineTwo">& Orders</span>
+          <div className="option">
+            <span className="optionLineOne">Returns</span>
+            <span className="optionLineTwo">& Orders</span>
           </div>
         </Link>
-        <div className="header__option">
-          <span className="header__optionLineOne">Your</span>
-          <span className="header__optionLineTwo">Prime</span>
-        </div>
+        {/* <div className="option">
+          <span className="optionLineOne">Your</span>
+          <span className="optionLineTwo">Prime</span>
+        </div> */}
         <Link to="/checkout">
-          <div className="header__optionBasket">
-            <ShoppingBasket />
-            <span className="header__optionLineTwo header__basketCount">0</span>
+          <div className="optionBasket">
+            <ShoppingCartIcon />
+            <span className="optionLineTwo basketCount">0</span>
           </div>
         </Link>
       </div>
