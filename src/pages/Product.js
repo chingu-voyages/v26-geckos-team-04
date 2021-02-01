85cce1270158b35475cece47de24060f6d040d92
Peter Hanley <hanley.peter@gmail.com>
2021-02-01 16:38:56 +0800

    Adjusted styling, added Header component to Product page

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -4,12 +4,17 @@ import ProductInformation from '../components/ProductInformation/ProductInformat
 import AddToShoppingCart from '../components/AddToShoppingCart/AddToShoppingCart';
 import { ProductContext } from '../contexts/StateProvider';
 import { Link } from "react-router-dom";
+import Header from '../components/Header/Header';
 
 const ProductPageStyles = styled.div`
     display: grid;
     grid-template-columns: 1fr;
     grid-gap: 10px;
     padding: 20px;
+    max-width: 1500px;
+    margin: 0 auto;
+    background-color: white;
+    min-height: 85vh;
 
     @media (min-width: 768px) {
         grid-template-columns: 1fr auto;
@@ -21,6 +26,7 @@ const ProductErrorPageStyles = styled.div`
     flex-direction: column;
     align-items: center;
     margin-top: 50px;
+    min-height: 80vh;
 `;
 
 export default function Product({ match }) {
@@ -34,15 +40,16 @@ export default function Product({ match }) {
 
     return (
         <>
-        {currentProduct?
-            <ProductPageStyles>
-                <ProductInformation product={currentProduct} />
-                <AddToShoppingCart product={currentProduct} />
-            </ProductPageStyles>
-        : <ProductErrorPageStyles>
-            <h2>Sorry! The product you are looking for has not been found!</h2>
-            <Link to="/">Back to Homepage</Link>
-        </ProductErrorPageStyles>}
+            <Header />
+            {currentProduct?
+                <ProductPageStyles>
+                    <ProductInformation product={currentProduct} />
+                    <AddToShoppingCart product={currentProduct} />
+                </ProductPageStyles>
+            : <ProductErrorPageStyles>
+                <h2>Sorry! The product you are looking for has not been found!</h2>
+                <Link to="/">Back to Homepage</Link>
+            </ProductErrorPageStyles>}
         </>
     )
 }
