8032ba1da38dcd2244501a2c25e906d4bee99c3e
Gonshz <1031gonshi@gmail.com>
2021-02-05 04:30:12 +0900

    payment page start

---
+++ src/pages/Payment.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const PaymentStyle = styled.section`
+    width: 100%;
+`;
+
+function Payment() {
+    return (
+        
+    )
+}
+
+export default Payment;
