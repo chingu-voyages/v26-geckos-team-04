d8ce91af6fd036c60baaff60b0fa68c0ef1f9e67
Gonshz <1031gonshi@gmail.com>
2021-02-05 05:37:05 +0900

    mid

--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -1,13 +1,92 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useStateValue } from './StateProvider';
+import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
 
 const PaymentStyle = styled.section`
+    width: 100%;
+    height: 100%;
+    background: #eaeded;
+    margin: 0;
+    padding: 14px 18px 18px;
+    box-sizing: border-box;
+    min-width: 998px;
+    @media screen and (max-width: 579px) {
+        background: #fff;
+        padding: 0;
+        min-width: 332px;
+    }
+`;
+const Container = styled.div`
+    width: 100%;
+    height: auto;
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    @media screen and (max-width: 579px) {
+        flex-direction: column;
+    }
+`;
+const Main = styled.main`
+    // margin-right: 320px;
+    padding-right: 20px;
+    flex-grow: 1;
+    @media screen and (max-width: 579px) {
+        padding: 0;
+        order: 2;
+    }
+`;
+const Aside = styled.aside`
+    margin-right: 20px;
+    @media screen and (max-width: 579px) {
+        order: 1;
+        border-bottom: solid 1px #ddd;
+        width: 100%;
+    }
+`;
+const Empty = styled.div`
+    width: auto;
+    background: #fff;
+    padding: 30px;
+    h2 { 
+        font-size: 32px;
+        font-weight: 300;
+    }
+    p {
+        padding: 0;
+        font-size: 14px;
+    }
+`;
+const Spacer = styled.div`
+    height: 300px;
     width: 100%;
 `;
 
 function Payment() {
+    const [{ basket, user }, dispatch] = useStateValue();
+    const stripe = useStripe();
+    const elements = useElements();
     return (
-        
+        <PaymentStyle>
+        <Container>
+            <Main>
+                {basket.length === 0 ? (
+                    <>
+                    <Empty>
+                        <h2>Your Amazon Cart is empty.</h2>
+                        <p>Check your Saved for later items below or <Link to='/' style={{textDecoration: 'none', color: '#007185'}}>continue shopping</Link>.</p>
+                    </Empty>
+                    <Spacer />
+                    </>
+                ) : (
+                    <ShoppingCartList products={basket} />
+                )}
+            </Main>
+            <Aside>
+                <ProceedToCheckout />
+            </Aside>
+        </Container>
+        </PaymentStyle>
     )
 }
 
